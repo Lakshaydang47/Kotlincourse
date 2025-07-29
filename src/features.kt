88d1547feb0549614facb8da23d5709a227422dd
Lakshaydang47 <lakshaydang764@gmail.com>
2025-07-29 23:33:49 +0530

    add features.kt

---
+++ src/features.kt
@@ -0,0 +1,35 @@
+//key features
+
+//1. complete null safety : as we know in java if we assign null to the object it
+// return exception where in kotlin it will not crash the program and it will not compile
+// the program which is helpfull
+
+// 2. we can use all the java libs. in kotlin as well
+
+
+// 3. kotlin has coroutines...which is similar as light weight threads in java
+
+// 4. less cpde in java
+
+// eg. of java and kotlin
+//  1st
+//java
+// theIsTheWholeNewClass myObject = new theIsTheWholeNewClass();
+
+// kotlin
+// var myObject  = theIsTheWholeNewClass()
+
+// 2nd
+//java
+// Button button = finfViewbyid(R.id.exampleButtone);
+//button.setOnClickListener(new View.OnClickListener() {
+//      public void onClick(View view){
+//
+//      }
+//});
+
+//kotlin
+//exampleButton.setOnClickListener {
+//
+// }
+
